de8668c247990e0bd8e659ebc752cea63d8572c0
Puneet Bajaj <142579024+Puneet-Bajaj-IITM@users.noreply.github.com>
2024-03-03 13:31:38 +0530

    Update config.py

--- src/config.py
+++ src/config.py
@@ -23,23 +23,8 @@ REMOVE_FILE_AFTER_MINS = 120 #every two hours
 # Instagram Configurations                                                                         #
 #--------------------------------------------------------------------------------------------------#
 
-# IS RUN REELS SCRAPER
-IS_ENABLED_REELS_SCRAPER = 1
-
 # IS RUN AUTO POSTER
 IS_ENABLED_AUTO_POSTER = 1
-
-# IS POST STORY
-IS_POST_TO_STORY = 1
-
-# Fetch LIMIT for scraper script
-FETCH_LIMIT = 10
-
-# Posting interval in Minutes
-POSTING_INTERVAL_IN_MIN = 15  # Every 15 Minutes
-
-# Scraper interval in Minutes
-SCRAPER_INTERVAL_IN_MIN = 720  # Every 12 hours
 
 # Instagram Username & Password
 USERNAME = "your_username"
@@ -62,27 +47,3 @@ DISABLE_COMMENTS = 0
 
 # HASHTAGS to add while Posting
 HASHTAGS = "#reels #shorts #likes #follow #Reels-AutoPilot"
-
-#--------------------------------------------------------------------------------------------------#
-# Youtube Configurations                                                                           #
-#--------------------------------------------------------------------------------------------------#
-
-# IS RUN YOUTUBE SCRAPER
-IS_ENABLED_YOUTUBE_SCRAPING = 1
-
-
-# IS RUN YOUTUBE SCRAPER
-YOUTUBE_SCRAPING_INTERVAL_IN_MINS = 120
-
-
-# YOUTUBE API KEY
-YOUTUBE_API_KEY = "YOUR_API_KEY"
-
-
-
-# YouTube Channel List short for scraping
-CHANNEL_LINKS = [
-    "https://www.youtube.com/@exampleChannleName."
-]
-
-
